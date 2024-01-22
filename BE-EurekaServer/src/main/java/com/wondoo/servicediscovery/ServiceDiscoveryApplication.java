442b03e2b3e1d48a13b0029375a094a6cef85e37
Sei Kim <workju1124@gmail.com>
2024-01-22 12:55:46 +0900

    Feat: Eureka Server 생성(#1)

---
+++ BE-EurekaServer/src/main/java/com/wondoo/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.wondoo.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+}
