e34ce6cd1963fb8b9e4489133e938e92ea5b91a6
Sei Kim <workju1124@gmail.com>
2024-01-22 13:20:25 +0900

    Feat: EnableEurekaServer(#1)

--- BE-EurekaServer/src/main/java/com/wondoo/servicediscovery/ServiceDiscoveryApplication.java
+++ BE-EurekaServer/src/main/java/com/wondoo/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.wondoo.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
