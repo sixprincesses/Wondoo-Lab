352ac5bcc54c7f2ccf93ea3a83358738ad639245
sirlyun <sirlyun23@gmail.com>
2024-01-23 09:11:44 +0900

    Refactor: 사용하지 않는 어노테이션 제거(#2)

--- BE-MemberService/src/main/java/com/wondoo/memberservice/global/SecurityConfig.java
+++ BE-MemberService/src/main/java/com/wondoo/memberservice/global/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.wondoo.memberservice.global;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- BE-MemberService/src/main/java/com/wondoo/memberservice/member/controller/MemberController.java
+++ BE-MemberService/src/main/java/com/wondoo/memberservice/member/controller/MemberController.java
@@ -8,7 +8,6 @@ import com.wondoo.memberservice.member.client.github.data.response.GithubUserInf
 import com.wondoo.memberservice.member.service.MemberSaveService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class MemberController {
 
     private final GithubClient githubClient;
