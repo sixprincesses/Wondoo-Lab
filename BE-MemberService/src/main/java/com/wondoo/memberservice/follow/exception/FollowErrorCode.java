1d190512b3193dd2dcdde294819a1ba6d34e49ee
sirlyun <sirlyun23@gmail.com>
2024-01-25 09:53:28 +0900

    Refactor: Error Message 수정

--- BE-MemberService/src/main/java/com/wondoo/memberservice/follow/exception/FollowErrorCode.java
+++ BE-MemberService/src/main/java/com/wondoo/memberservice/follow/exception/FollowErrorCode.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 public enum FollowErrorCode implements ErrorCode {
 
-    FOLLOW_NOT_FOUND(400, "Follow_001", "follow not found"),
-    FOLLOW_BAD_REQUEST(400, "Follow_002", "cannot follow yourself");
+    FOLLOW_NOT_FOUND(400, "Follow_001", "팔로우 한 적이 없는 사용자입니다."),
+    FOLLOW_BAD_REQUEST(400, "Follow_002", "스스로 팔로우 할 수 없습니다.");
 
     private final int status;
     private final String code;
