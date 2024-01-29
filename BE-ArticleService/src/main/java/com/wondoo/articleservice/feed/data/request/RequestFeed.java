af7ac812a70c6eb1e3581ca3ceeff7084f00cfb2
junhochoi-dev <cjh970422@gmail.com>
2024-01-29 21:40:05 +0900

    Feat: Builder 추가(#15)

--- BE-ArticleService/src/main/java/com/wondoo/articleservice/feed/data/request/RequestFeed.java
+++ BE-ArticleService/src/main/java/com/wondoo/articleservice/feed/data/request/RequestFeed.java
@@ -1,11 +1,14 @@
 package com.wondoo.articleservice.feed.data.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
 @Getter
 @Setter
 public class RequestFeed {
--- BE-ArticleService/src/main/java/com/wondoo/articleservice/feed/data/response/ResponseFeed.java
+++ BE-ArticleService/src/main/java/com/wondoo/articleservice/feed/data/response/ResponseFeed.java
@@ -1,11 +1,13 @@
 package com.wondoo.articleservice.feed.data.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
 @Getter
 @Setter
 public class ResponseFeed {
