6cf60fdd9fa353e6773fed0facd4c5bf5ae7f6b5
junhochoi-dev <cjh970422@gmail.com>
2024-01-29 19:58:05 +0900

    Feat: 수정, 삭제, 생성 완료 / 전체조회 미완료 (#15)

--- BE-ArticleService/src/main/java/com/wondoo/articleservice/feed/service/FeedService.java
+++ BE-ArticleService/src/main/java/com/wondoo/articleservice/feed/service/FeedService.java
@@ -1,4 +1,68 @@
 package com.wondoo.articleservice.feed.service;
 
+import com.wondoo.articleservice.feed.data.request.RequestFeed;
+import com.wondoo.articleservice.feed.domain.Feed;
+import com.wondoo.articleservice.feed.domain.FeedMember;
+import com.wondoo.articleservice.feed.repository.FeedMemberRepository;
+import com.wondoo.articleservice.feed.repository.FeedRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FeedService {
+    private final FeedRepository feedRepository;
+    private final FeedMemberRepository feedMemberRepository;
+
+    public void getFeeds(String feedId) {
+        // FeedMember에서 index별로 가져오기 g
+    }
+
+    @Transactional
+    public void createFeed(RequestFeed requestDto) {
+        String feedId = UUID.randomUUID().toString();
+
+        FeedMember feedMember = FeedMember.builder()
+                .feedId(feedId)
+                .memberId(requestDto.getMemberId())
+                .build();
+        feedMemberRepository.save(feedMember);
+
+        long totalTime = 0;
+        for (List<LocalDateTime> timelog : requestDto.getTimeLogs()) {
+            Duration duration = Duration.between(timelog.get(0), timelog.get(1));
+            totalTime += duration.getSeconds();
+        }
+        Feed feed = Feed.builder()
+                .id(feedId)
+                .memberId(requestDto.getMemberId())
+                .title(requestDto.getTitle())
+                .content(requestDto.getContent())
+                .TimeLogs(requestDto.getTimeLogs())
+                .totalTime(totalTime)
+                .build();
+        feedRepository.save(feed);
+    }
+
+    @Transactional
+    public void updateFeed(RequestFeed requestDto) {
+        Optional<Feed> feed = feedRepository.findById(requestDto.getFeedId());
+        if (feed.isPresent()) {
+            feed.get().updateFeed(requestDto.getTitle(), requestDto.getContent());
+            feedRepository.save(feed.get());
+        }
+    }
+
+    @Transactional
+    public void deleteFeed(String feedId) {
+        feedRepository.deleteById(feedId);
+    }
 }
