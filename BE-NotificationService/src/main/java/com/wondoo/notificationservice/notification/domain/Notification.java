7c552996775af39110cea93c6f01a03efdee8811
sirlyun <sirlyun23@gmail.com>
2024-01-30 09:46:13 +0900

    Feat: Kafka Consumer 설정(#16)
    - Member Service 관련 Topic 구독
    - Kafka 이벤트 수신 시 SSE 구독자에게 메시지 전송

--- BE-NotificationService/src/main/java/com/wondoo/notificationservice/notification/domain/Notification.java
+++
@@ -1,2 +0,0 @@
-package com.wondoo.notificationservice.notification.domain;public class Notification {
-}
--- BE-NotificationService/src/main/java/com/wondoo/notificationservice/notification/util/KafkaConsumer.java
+++ BE-NotificationService/src/main/java/com/wondoo/notificationservice/notification/util/KafkaConsumer.java
@@ -1,2 +1,24 @@
-package com.wondoo.notificationservice.notification.util;public class KafkaConsumer {
+package com.wondoo.notificationservice.notification.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wondoo.notificationservice.notification.data.message.FollowMessage;
+import com.wondoo.notificationservice.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+    private final NotificationService notificationService;
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "${message.topic.name}", groupId = "member", containerFactory = "kafkaListener")
+    public void consumeMessage(String message) throws IOException {
+        FollowMessage followMessage = objectMapper.readValue(message, FollowMessage.class);
+        notificationService.notify(followMessage.targetId(), followMessage.content());
+    }
 }
