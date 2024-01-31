8b5d2dc50e9f27329dfc0d0a3ef24e3639df46e1
junhochoi-dev <cjh970422@gmail.com>
2024-01-31 13:52:03 +0900

    Feat: ExëApiResponse 반영(#22)

--- BE-StorageService/src/main/java/com/wondoo/storageservice/storage/controller/StorageController.java
+++ BE-StorageService/src/main/java/com/wondoo/storageservice/storage/controller/StorageController.java
@@ -1,5 +1,7 @@
 package com.wondoo.storageservice.storage.controller;
 
+import com.wondoo.storageservice._global.data.ApiResponse;
+import com.wondoo.storageservice._global.data.StatusCode;
 import com.wondoo.storageservice.storage.data.request.FileRequest;
 import com.wondoo.storageservice.storage.data.request.FilesRequest;
 import com.wondoo.storageservice.storage.data.response.FileResponse;
@@ -15,6 +17,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/storage-service")
 @RequiredArgsConstructor
 public class StorageController {
@@ -22,26 +25,30 @@ public class StorageController {
     private final StorageService storageService;
 
     @PostMapping("/file")
-    public ResponseEntity<FileResponse> saveFile(@RequestPart("filesource") MultipartFile file) throws IOException {
+    public ResponseEntity<ApiResponse<FileResponse>> saveFile(@RequestPart("filesource") MultipartFile file) throws IOException {
         FileResponse fileResponse = storageService.saveFile(file);
-        return ResponseEntity.status(HttpStatus.CREATED).body(fileResponse);
+        ApiResponse<FileResponse> response = new ApiResponse<>(StatusCode.SUCCESS_INSERT, fileResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PostMapping("/files")
-    public ResponseEntity<FilesResponse> saveFiles(@RequestPart("filesources") List<MultipartFile> files) throws IOException {
+    public ResponseEntity<ApiResponse<FilesResponse>> saveFiles(@RequestPart("filesources") List<MultipartFile> files) throws IOException {
         FilesResponse filesResponse = storageService.saveFiles(files);
-        return ResponseEntity.status(HttpStatus.CREATED).body(filesResponse);
+        ApiResponse<FilesResponse> response = new ApiResponse<>(StatusCode.SUCCESS_INSERT, filesResponse);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @DeleteMapping("/file")
-    public ResponseEntity<?> deleteFile(@RequestBody FileRequest file) {
+    public ResponseEntity<ApiResponse<Void>> deleteFile(@RequestBody FileRequest file) {
         storageService.deleteFile(file);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        ApiResponse<Void> response = new ApiResponse<>(StatusCode.SUCCESS_DELETE, null);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @DeleteMapping("/files")
-    public ResponseEntity<?> deleteFiles(@RequestBody FilesRequest files) {
+    public ResponseEntity<ApiResponse<Void>> deleteFiles(@RequestBody FilesRequest files) {
         storageService.deleteFiles(files);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        ApiResponse<Void> response = new ApiResponse<>(StatusCode.SUCCESS_DELETE, null);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
