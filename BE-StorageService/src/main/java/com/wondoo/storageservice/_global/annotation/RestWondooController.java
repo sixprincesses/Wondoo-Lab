f1f1b7bfc0f7bc4d0125c931b008f0b69fcf1fe5
junhochoi-dev <cjh970422@gmail.com>
2024-01-31 14:11:27 +0900

    Feat: CrossOrigin(#22)

--- BE-StorageService/src/main/java/com/wondoo/storageservice/_global/annotation/RestWondooController.java
+++ BE-StorageService/src/main/java/com/wondoo/storageservice/_global/annotation/RestWondooController.java
@@ -2,11 +2,12 @@ package com.wondoo.storageservice._global.annotation;
 
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.lang.annotation.*;
-
+@CrossOrigin("*")
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
