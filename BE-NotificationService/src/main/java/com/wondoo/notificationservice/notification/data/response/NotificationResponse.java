4f756ba8d72392258b44ce5d72f9fc29d126a1fe
sirlyun <sirlyun23@gmail.com>
2024-02-05 15:12:59 +0900

    Feat: 반환 형식 정의(#36)
    - 안읽은 알림 개수 반환
    - 알림 데이터 반환

--- BE-NotificationService/src/main/java/com/wondoo/notificationservice/notification/data/response/NotificationResponse.java
+++ BE-NotificationService/src/main/java/com/wondoo/notificationservice/notification/data/response/NotificationResponse.java
@@ -1,2 +1,17 @@
-package com.wondoo.notificationservice.notification.data.response;public record NotificationResponse() {
+package com.wondoo.notificationservice.notification.data.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+
+@Builder
+public record NotificationResponse(
+        @JsonProperty("type")
+        String type,
+        @JsonProperty("read")
+        Boolean read,
+        @JsonProperty("content")
+        String content,
+        @JsonProperty("time")
+        Long time
+) {
 }
