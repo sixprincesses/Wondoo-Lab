8371b87c815dca7f9f27dc9ee9d69c18c2a3fed5
sirlyun <sirlyun23@gmail.com>
2024-02-08 14:44:39 +0900

    Refator: 기본 설정 변경
    - CrossOrigin 추가
    - Swagger 추가

--- BE-NotificationService/src/main/java/com/wondoo/notificationservice/global/annotation/RestWondooController.java
+++ BE-NotificationService/src/main/java/com/wondoo/notificationservice/global/annotation/RestWondooController.java
@@ -2,6 +2,7 @@ package com.wondoo.notificationservice.global.annotation;
 
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,6 +14,7 @@ import java.lang.annotation.*;
 @Controller
 @ResponseBody
 @RequestMapping("/notification-service")
+@CrossOrigin("*")
 public @interface RestWondooController {
     @AliasFor(
             annotation = Controller.class
